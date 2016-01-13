e8aafa14068c29e3bf96772fb1d1a4577f2d31e0
张国荣 <zgr@localhost>
2016-01-13 18:57:05 +0800

    Initial Commit

---
+++ XmppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XmppDemo
+//
+//  Created by 张国荣 on 16/1/13.
+//  Copyright © 2016年 tt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
